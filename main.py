0363b43dc1a52bc2c2b62822afccfdcbbe6b66e7
Root <inventoxz@gmail.com>
2023-02-13 02:38:15 +0100

    follow_pointers()

--- main.py
+++ main.py
@@ -6,11 +6,16 @@ faraway = Pymem("faraway.exe")
 def addr_of_pointer(offset):
 	return faraway.read_int(offset)
 
+def follow_pointers(base_offset, *pointer_offsets):
+	addr = addr_of_pointer(faraway.base_address + base_offset)
+	for offset in pointer_offsets[:-1]:
+		addr = addr_of_pointer(addr + offset)
+	return addr + pointer_offsets[-1]
 
-health_addr = addr_of_pointer(addr_of_pointer(faraway.base_address + 0x5a0cc8) + 0x30) + 0x1c
+health_addr = follow_pointers(0x5a0cc8, 0x30, 0x1c)
 print("health", faraway.read_float(health_addr))
 
-food_addr = addr_of_pointer(addr_of_pointer(faraway.base_address + 0x5a0cc8) + 0x30) + 0x18
+food_addr = follow_pointers(0x5a0cc8, 0x30, 0x18)
 print("food", faraway.read_float(health_addr))
 
 while True:
